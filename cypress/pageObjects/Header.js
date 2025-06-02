382c437b57bec7ab5e7e5e814c68735e861d5fb8
lenka0376 <elena.goren@gmail.com>
2025-06-01 21:23:42 -0500

    tc7

--- cypress/pageObjects/Header.js
+++ cypress/pageObjects/Header.js
@@ -1,11 +1,18 @@
 import SignupLoginPage from "./SignupLoginPage";
+import TestCasesPage from "./TestCasesPage";
 
 class Header {
     getSignupLoginButton = () => cy.get('a[href="/login"]');
+    getTestCasesButton =() =>cy.get('header a[href="/test_cases"]');
     
     clickSignupLoginButton(){
         this.getSignupLoginButton().click();
         return new SignupLoginPage();
+    }
+    
+    clickTestCasesButton(){
+        this.getTestCasesButton().click();
+        return new TestCasesPage();
     }
 
 }
---
+++ cypress/pageObjects/TestCasesPage.js
@@ -0,0 +1,5 @@
+class TestCases {
+    getTestCaseTitle = () => cy.get('#form h2'); 
+}
+
+export default TestCases;
