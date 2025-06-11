853f6ae525963b01771ddc5029fbec6b0c60c5bc
Alina <alina.sinyagina@gmail.com>
2025-06-10 19:33:22 -0700

    fixed

--- cypress/e2e/loginUser.cy.js
+++ cypress/e2e/loginUser.cy.js
@@ -1,20 +1,20 @@
 /// <reference types = "cypress"/>
 import Header from "../pageObjects/Header";
-import SignupLogin from "../pageObjects/SignupLoginPage";
+import SignupLoginPage from "../pageObjects/SignupLoginPage";
 import signupLoginPageData from "../fixtures/signupLoginPageData.json";
 
 describe("Login User", ()=> {
     const header = new Header();
-    const signupLogin = new SignupLogin();
+    const signupLoginPage = new SignupLoginPage();
 
     it("TC_3 | Login User with incorrect email and password", ()=> {
         cy.visit('/');
         header.clickSignupLoginButton();
-        signupLogin.getLoginToYourAccountTitle().should('be.visible');
-        signupLogin.getEmailAddressField().type(signupLoginPageData.incorrectEmail);
-        signupLogin.getPasswordField().type(signupLoginPageData.incorrectPassword);
-        signupLogin.getLoginButton().click();
-        signupLogin.getErrorMessageIncorectCredentials()
+        signupLoginPage.getLoginToYourAccountTitle().should('be.visible');
+        signupLoginPage.inputEmailAddress(signupLoginPageData.incorrectEmail);
+        signupLoginPage.inputPassword(signupLoginPageData.incorrectPassword);
+        signupLoginPage.clickLoginButton();
+        signupLoginPage.getErrorMessageIncorectCredentials()
             .should('have.text', signupLoginPageData.errorMessageIncorectCredentials)
             .and('have.css', 'color', 'rgb(255, 0, 0)');
     })
