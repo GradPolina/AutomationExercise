d55232ebb5481db027f36192d842eaf668c2de55
Alina Syniagina <56331881+asyniagina@users.noreply.github.com>
2025-06-16 19:43:35 -0700

    added cypress-real-events and TC_12.2 (#33)

--- cypress/pageObjects/ProductsPage.js
+++ cypress/pageObjects/ProductsPage.js
@@ -1,4 +1,5 @@
-import ProductPage from "./ProductDetailsPage"
+import ProductPage from "./ProductDetailsPage";
+import ViewCart from "./ViewCartPage";
 
 class ProductsPage {
     getAllProductsListTitle = () => cy.get('.features_items >.title'); 
@@ -7,6 +8,10 @@ class ProductsPage {
     getSearch = () => cy.get('#search_product');
     getSearchSubmitButton = () => cy.get('#submit_search');
     getSearchResultsTitle = () => cy.get('h2.title');
+    getProductsByIndex = (index) => cy.get('.single-products').eq(index);
+    getAddItemToCartByProductId = (id) => cy.get(`.overlay-content a[data-product-id="${id}"] .fa-shopping-cart`);
+    getContinueShoppingButton = () => cy.get('.btn-success');
+    getViewCartLinkPopUp = () => cy.get('.modal-body a[href*="cart"]');
 
     clickViewproduct1(){
         this.getProductViewProduct1().click();
@@ -21,6 +26,26 @@ class ProductsPage {
     inputSearchValue(text){
         this.getSearch().type(text);
         return this;
+    }
+
+    hoverOnProductByIndex(index) {
+        this.getProductsByIndex(index).scrollIntoView().realHover();
+        return this;
+    }
+
+    clickAddToCartById(id) {
+        this.getAddItemToCartByProductId(id).click();
+        return this;
+    }
+
+    clickContinueShopping() {
+        this.getContinueShoppingButton().click();
+        return this;
+    }
+
+    clickViewCartLinkPopUp() {
+        this.getViewCartLinkPopUp().click();
+        return new ViewCart();
     }
 
 }
