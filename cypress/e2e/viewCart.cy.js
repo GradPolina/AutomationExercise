8322b6aae8a720d20c0a36b0d40c1e6b76b4f401
Alina <alina.sinyagina@gmail.com>
2025-06-16 17:17:55 -0700

    added cypress-real-events and TC_12.2

--- cypress/e2e/viewCart.cy.js
+++ cypress/e2e/viewCart.cy.js
@@ -1,12 +1,16 @@
 /// <reference types = "cypress"/>
 
 import Header from "../pageObjects/Header";
+import ProductsPage from "../pageObjects/ProductsPage";
 import ViewCart from "../pageObjects/ViewCartPage";
 import viewCartPageData from "../fixtures/viewCartPageData.json";
+
+import "cypress-real-events/support";
 
 describe("View Cart Page", ()=>{
     const header = new Header();
     const viewCart = new ViewCart();
+    const productsPage = new ProductsPage();
 
     it('TC_12.1|Verify Cart is empty before shopping', ()=>{
         cy.visit('/');
@@ -15,5 +19,28 @@ describe("View Cart Page", ()=>{
             .should('be.visible')
             .and('have.text', viewCartPageData.emptyCartMessage);
     });
+
+    it('TC_12.2|Add two products in the cart', () => {
+        cy.visit('/');
+        header.clickProductsButton();
+        cy.url().should('include', '/products');
+        productsPage
+            .hoverOnProductByIndex(0)
+            .clickAddToCartById(1)
+            .clickContinueShopping()
+            .hoverOnProductByIndex(1)
+            .clickAddToCartById(2)
+            .clickViewCartLinkPopUp()
+        viewCart.verifyNumberOfItemsInCart(2)
+        viewCart.getCartRows().each(($row, index) => {
+            const expected = viewCartPageData.addedTwoItems[index];
+
+            viewCart.getRowProductId($row).should('equal', expected.id);
+            viewCart.getProductName($row).should('have.text', expected.description);
+            viewCart.getProductPrice($row).should('have.text', expected.price);
+            viewCart.getProductQuantity($row).should('have.text', expected.quantity);
+            viewCart.getProductTotalPrice($row).should('have.text', expected.price_total);
+        });
+    })
 
 });
--- cypress/pageObjects/ViewCartPage.js
+++ cypress/pageObjects/ViewCartPage.js
@@ -1,5 +1,19 @@
 class ViewCart {
     getEmptyCartMessage = () => cy.get('#empty_cart b');
+    getCartProductCell = () => cy.get('#cart_info_table .cart_product');
+    getCartRows = () => cy.get('#cart_info_table tbody tr');
+    getRowProductId = ($row) => cy.wrap($row).invoke('attr', 'id');
+    getProductName = ($row) => cy.wrap($row).find('.cart_description h4 a');
+    getProductPrice = ($row) => cy.wrap($row).find('.cart_price p');
+    getProductQuantity = ($row) => cy.wrap($row).find('.cart_quantity .disabled');
+    getProductTotalPrice = ($row) => cy.wrap($row).find('.cart_total_price');
+
+    verifyNumberOfItemsInCart(expectedCount) {
+      this.getCartProductCell().then($items => {
+        expect($items.length).to.equal(expectedCount);
+      });
+    }
+
 }
 
 export default ViewCart;
