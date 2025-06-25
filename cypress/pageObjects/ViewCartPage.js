3d0bb79f1718dcbe976a94300d032891cf5562ac
GradPolina <Gradinar_polina@yahoo.com>
2025-06-24 23:28:49 -0500

    TC 13 Verify Product quantity in Cart

--- cypress/pageObjects/ViewCartPage.js
+++ cypress/pageObjects/ViewCartPage.js
@@ -4,13 +4,14 @@ class ViewCart {
   getSubscriptionEmail = () => cy.get("#susbscribe_email");
   getSubscriptionArrow = () => cy.get("#subscribe");
   getSuccessMessage = () => cy.get("#success-subscribe");
-  getCartProductCell = () => cy.get('#cart_info_table .cart_product');
-  getCartRows = () => cy.get('#cart_info_table tbody tr');
-  getRowProductId = ($row) => cy.wrap($row).invoke('attr', 'id');
-  getProductName = ($row) => cy.wrap($row).find('.cart_description h4 a');
-  getProductPrice = ($row) => cy.wrap($row).find('.cart_price p');
-  getProductQuantity = ($row) => cy.wrap($row).find('.cart_quantity .disabled');
-  getProductTotalPrice = ($row) => cy.wrap($row).find('.cart_total_price');
+  getCartProductCell = () => cy.get("#cart_info_table .cart_product");
+  getCartRows = () => cy.get("#cart_info_table tbody tr");
+  getRowProductId = ($row) => cy.wrap($row).invoke("attr", "id");
+  getProductName = ($row) => cy.wrap($row).find(".cart_description h4 a");
+  getProductPrice = ($row) => cy.wrap($row).find(".cart_price p");
+  getProductQuantity = ($row) => cy.wrap($row).find(".cart_quantity .disabled");
+  getProductTotalPrice = ($row) => cy.wrap($row).find(".cart_total_price");
+  getProductCount = () => cy.get(".cart_quantity button");
 
   inputSubscriptionEmail(text) {
     this.getSubscriptionEmail().type(text);
@@ -23,11 +24,10 @@ class ViewCart {
   }
 
   verifyNumberOfItemsInCart(expectedCount) {
-    this.getCartProductCell().then($items => {
+    this.getCartProductCell().then(($items) => {
       expect($items.length).to.equal(expectedCount);
     });
   }
-  
 }
 
 export default ViewCart;
