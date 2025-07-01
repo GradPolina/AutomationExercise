7fb8e04551985817c59fad7adb0c34a3a58efea3
Alina <alina.sinyagina@gmail.com>
2025-07-01 10:23:45 -0400

    TC_25

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,6 +1,7 @@
 /// <reference types = "cypress"/>
 
 import HomePage from "../pageObjects/HomePage";
+import homePageData from "../fixtures/homePageData.json"
 
 describe("Home", () => {
   const home = new HomePage();
@@ -15,4 +16,13 @@ describe("Home", () => {
       "You have been successfully subscribed!"
     );
   });
+
+  it("TC_25|Verify Scroll Up using 'Arrow' button and Scroll Down functionality", () => {
+    cy.visit("/");
+    cy.url().should("include", "automationexercise");
+    cy.scrollTo('bottom');
+    home.getSubscription().should("be.visible");
+    home.clickArrowButton();
+    home.getCarouselHeader().should("be.visible").and("have.text", homePageData.carouselHeader);
+  })
 });
