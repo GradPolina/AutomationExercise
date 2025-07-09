e6e2d5641526941419cb5e2dde352d46ef996d15
Alina Syniagina <56331881+asyniagina@users.noreply.github.com>
2025-07-08 22:08:45 -0400

    added grep plugin (#63)

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,15 @@
 const { defineConfig } = require("cypress");
+const cypressGrepPlugin = require('@cypress/grep/src/plugin');
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://automationexercise.com/',
     setupNodeEvents(on, config) {
+      cypressGrepPlugin(config, {
+        tags: true,            // enables the { tags: [...] }
+        grepFilterSpecs: true, // skip files with no matching tags
+      });
+      return config;
       // implement node event listeners here
     },
   },
