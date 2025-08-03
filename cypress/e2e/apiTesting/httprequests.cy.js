7924aafb24c097dc26210bcf021aaad59447ab82
GradPolina <Gradinar_polina@yahoo.com>
2025-08-03 11:04:08 -0500

    Restore changes after deleted branch

--- cypress/e2e/apiTesting/httprequests.cy.js
+++
@@ -1,9 +0,0 @@
-/// <reference types = "cypress"/>
-
-describe("HTTP Requests", () => {
-  it("TC API 1: Get All Products list", { tags: ["@api"] }, () => {
-    cy.request("GET", "https://automationexercise.com/api/productsList")
-      .its("status")
-      .should("equal", 200);
-  });
-});
